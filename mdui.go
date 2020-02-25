c132e91bd4ca55d6a6928f72cc5d9b6babedd0de
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-02-25 17:38:30 +0800

    Just a Backup

--- mdui.go
+++ mdui.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func HandleCss(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/css")
-	w.Write([]byte(Str_MduiMinCss))
+	w.Write(Bytes_MduiMinCss)
 }
 
 func HandleJs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/javascript")
-	w.Write([]byte(Str_MduiMinJs))
+	w.Write(Bytes_MduiMinJs)
 }
