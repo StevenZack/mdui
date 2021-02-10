d158abb94c469c1ff79103018e7c6040b339cfc0
stevenzack <stevenzack@qq.com>
2021-02-10 22:21:37 +0800

    embed

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/StevenZack/mdui
+
+go 1.16
--- mdui.go
+++ mdui.go
@@ -1,29 +1,6 @@
 package mdui
 
-import (
-	"net/http"
+import "embed"
 
-	"github.com/StevenZack/mux"
-
-	"github.com/StevenZack/mdui/views"
-)
-
-// HandleAll e.g prefix="/res/"
-func HandleAll(server *mux.Server, prefix string) {
-	server.HandleWoff(prefix+"fonts/roboto/Roboto-Medium.woff", views.Bytes_RobotoMediumWoff)
-	server.HandleWoff(prefix+"fonts/roboto/Roboto-Medium.woff2", views.Bytes_RobotoMediumWoff2)
-	server.HandleWoff(prefix+"icons/material-icons/MaterialIcons-Regular.woff", views.Bytes_MaterialiconsRegularWoff)
-	server.HandleWoff(prefix+"icons/material-icons/MaterialIcons-Regular.woff2", views.Bytes_MaterialiconsRegularWoff2)
-	server.HandleCss(prefix+"css/mdui.min.css", views.Bytes_MduiMinCss)
-	server.HandleJs(prefix+"js/mdui.min.js", views.Bytes_MduiMinJs)
-}
-
-func HandleCss(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/css")
-	w.Write(views.Bytes_MduiMinCss)
-}
-
-func HandleJs(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/javascript")
-	w.Write(views.Bytes_MduiMinJs)
-}
+//go:embed res
+var Res embed.FS
